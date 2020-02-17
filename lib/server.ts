bd19c0294edc0b57029795b2c191c283acc5ad8b
Nicola Dall'Asen <nicola.dallasen@gmail.com>
2020-02-17 21:48:58 +0100

    change api endpoint

--- lib/server.ts
+++ lib/server.ts
@@ -1,7 +1,8 @@
 import express = require('express');
 
 const app: express.Application = require('./app');
+const PORT: number = 5000;
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 })
