449b7bfe6b3efa82250015109cf859096086e8ee
Nicola Dall'Asen <nicola.dallasen@gmail.com>
2020-02-18 08:24:53 +0100

    basic react and material

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import {AppBar, Toolbar, IconButton, Typography, Button} from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
-function App() {
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    title: {
+      flexGrow: 1
+    }
+  })
+);
+
+type AppProps = {};
+
+type TestObj = {
+  name: 'string'
+};
+
+const App = (props: AppProps) => {
+  const classes = useStyles();
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let result = await fetch(
+        `/api/test`
+      );
+
+      if (result.ok) {
+        let d = await result.json();
+        setData(d);
+      }
+    }
+
+    fetchData();
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton edge="start" color="inherit" aria-label="menu">
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            TODO
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <h1>Hello</h1>
+      {data && data.map((d: TestObj) =>
+        <p>{d.name}</p>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
